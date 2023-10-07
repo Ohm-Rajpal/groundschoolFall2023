a15cada49b0dbfb9d6a031d4705255b201e94bcf
Ohm-Rajpal <ohm_rajpal@berkeley.edu>
2023-10-06 20:38:57 -0700

    Began exploring OpenCV

--- ImageArgumentParser.py
+++ ImageArgumentParser.py
@@ -1,10 +1,13 @@
 import argparse
 
+
 def main(arguments):
     print("Starting main() function")
 
+
 # This if statement will take a path to an image if you attach the flag -ip before the call.
-# It will then run the main function passing in the arguments as a list, so to access the image path, you must call arguments[0]
+# It will then run the main function passing in the arguments as a list, so to access the image path,
+# you must call arguments[0]
 # You can import your solution and then call it in the main() function
 # Example call in terminal to run this script: python3 ImageArgumentParser.py -ip "./test_images/color.png"
 if __name__ == "__main__":
